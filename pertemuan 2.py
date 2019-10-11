c515c210bfab74ed12adf16afdbd7325828d24a0
Rifai <rifaislamet1509@gmail.com>
2019-10-11 16:37:03 +0700

    kecepatan tanpa fungsi

--- pertemuan 2.py
+++ pertemuan 2.py
@@ -2,3 +2,9 @@ nama ='Rifai'
 program ='Gerak Lurus'
 
 print(f'program {program} oleh {nama}')
+
+jarak= 1000
+waktu = 5 *60
+kecepatan = jarak / waktu
+print(f'jarak= {jarak/1000} km ditempuh dalam waktu ={waktu/60} menit')
+print(f'sehingga kecepatan ={kecepatan} m/s')
