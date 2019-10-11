51c8c5d809f75dff07379ccd80020d36ac105f8b
Rifai1509 <56153904+Rifai1509@users.noreply.github.com>
2019-10-11 17:08:45 +0700

    Delete tugas1.py

--- tugas1.py
+++
@@ -1,9 +0,0 @@
-def hitung_kecepatan(massa,percepatan):
-    gaya = massa*percepatan
-    print('jarak= {jarak / 1000} km ditempuh dalam waktu ={waktu / 60} menit')
-    print(f'sehingga kecepatan ={kecepatan} m/s')
-    return kecepatan
-# jarak = 1000
-# waktu = 5*60
-kecepatan =hitung_kecepatan(1000, 5*60)
-kecepatan =hitung_kecepatan(5000, 15*60)
